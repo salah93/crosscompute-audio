34a59906113a6ca66718bc81b75cd01d203701fa
salah ahmed <salah93@crosscompute.com>
2017-03-09 21:20:09 +0000

    add skeleton for plugin

---
+++ README.rst
@@ -0,0 +1,5 @@
+.. image:: https://travis-ci.org/salah93/crosscompute-audio.svg?branch=master
+    :target: https://travis-ci.org/salah93/crosscompute-audio
+
+CrossCompute Audio
+====================
---
+++ crosscompute_audio/__init__.py
@@ -0,0 +1,17 @@
+from crosscompute.scripts.serve import get_result_file_url
+from crosscompute.types import DataType
+
+
+class AudioType(DataType):
+
+    suffixes = 'audio',
+    formats = 'mp3', 'mp4', 'ogg'
+    template = 'crosscompute_audio:type.jinja2'
+
+    @classmethod
+    def load(Class, path):
+        return path
+
+    @classmethod
+    def render(Class, path):
+        return get_result_file_url(path)
